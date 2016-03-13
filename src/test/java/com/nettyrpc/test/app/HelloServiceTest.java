24425e5d38aa678d859c5ebc45b9f7855dc4a0de
luxiaoxun <luxiaoxun001@163.com>
2016-03-13 13:53:17 +0800

    Init commit

---
+++ src/test/java/com/nettyrpc/test/app/HelloServiceTest.java
@@ -0,0 +1,33 @@
+package com.nettyrpc.test.app;
+
+import com.nettyrpc.client.RpcProxy;
+import com.nettyrpc.test.client.HelloService;
+import com.nettyrpc.test.client.Person;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:client-spring.xml")
+public class HelloServiceTest {
+
+    @Autowired
+    private RpcProxy rpcProxy;
+
+    @Test
+    public void helloTest1() {
+        HelloService helloService = rpcProxy.create(HelloService.class);
+        String result = helloService.hello("World");
+        Assert.assertEquals("Hello! World", result);
+    }
+
+    @Test
+    public void helloTest2() {
+        HelloService helloService = rpcProxy.create(HelloService.class);
+        String result = helloService.hello(new Person("Yong", "Huang"));
+        Assert.assertEquals("Hello! Yong Huang", result);
+    }
+}
